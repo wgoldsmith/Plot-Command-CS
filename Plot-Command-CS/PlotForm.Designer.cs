306b91df110b3f6e6a92ce1e56ac29c8ccbce064
wgoldsmith <winstongoldsmith@hotmail.com>
2017-04-07 13:41:58 -0700

    changed default date and time. added paper status group box to form

--- Plot-Command-CS/PlotForm.Designer.cs
+++ Plot-Command-CS/PlotForm.Designer.cs
@@ -28,14 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.paperStatGroupBox = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
+            // 
+            // paperStatGroupBox
+            // 
+            this.paperStatGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.paperStatGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.paperStatGroupBox.Name = "paperStatGroupBox";
+            this.paperStatGroupBox.Size = new System.Drawing.Size(300, 100);
+            this.paperStatGroupBox.TabIndex = 0;
+            this.paperStatGroupBox.TabStop = false;
+            this.paperStatGroupBox.Text = "Paper Status";
             // 
             // PlotForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(284, 301);
+            this.ClientSize = new System.Drawing.Size(324, 261);
+            this.Controls.Add(this.paperStatGroupBox);
             this.Name = "PlotForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "PlotForm";
@@ -44,5 +57,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox paperStatGroupBox;
     }
 }
